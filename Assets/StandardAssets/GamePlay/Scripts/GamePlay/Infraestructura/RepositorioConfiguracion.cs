070ffacfda3d7ebc04cf5a5a837b2dddbfe4f7f3
Matias Nievas Soto <matytaa@gmail.com>
2020-10-12 18:45:09 -0300

    Dar configuracion de persona segun la demanda

--- Assets/StandardAssets/GamePlay/Scripts/GamePlay/Infraestructura/RepositorioConfiguracion.cs
+++ Assets/StandardAssets/GamePlay/Scripts/GamePlay/Infraestructura/RepositorioConfiguracion.cs
@@ -1,18 +1,34 @@
 ﻿using Scripts.GamePlay.Vistas;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
 namespace Scripts.GamePlay.Infraestructura
 {
     public class RepositorioConfiguracion
     {
         private ConfiguracionGeneral configuracion;
-        
+        private List<ConfiguracionDePersona> listaDeConfiguracionDePersonas;
+
         public RepositorioConfiguracion(ConfiguracionGeneral configuracion)
         {
             this.configuracion = configuracion;
+            ClonarListaDeConfiguracionDePersonas();
         }
 
         public ConfiguracionDePersona DarConfiguracion()
         {
-            return null;
+            if(listaDeConfiguracionDePersonas.Count() == 0)
+                ClonarListaDeConfiguracionDePersonas();
+                
+            var configuracionDePersona = listaDeConfiguracionDePersonas.First();
+            listaDeConfiguracionDePersonas.RemoveAt(0);
+            return configuracionDePersona;
+        }
+
+        private void ClonarListaDeConfiguracionDePersonas(){
+            listaDeConfiguracionDePersonas = new List<ConfiguracionDePersona>(configuracion.DarConfiguracionesDePersona());
         }
     }
 }
