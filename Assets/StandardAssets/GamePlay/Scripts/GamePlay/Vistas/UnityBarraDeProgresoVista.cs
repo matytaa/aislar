e879906d940bec8b17c1445dc61dcaa6d5779ff8
Matias Nievas Soto <matytaa@gmail.com>
2020-10-13 21:17:59 -0300

    Primer concepto de la barra de progreso

---
+++ Assets/StandardAssets/GamePlay/Scripts/GamePlay/Vistas/UnityBarraDeProgresoVista.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+
+namespace Scripts.GamePlay.Vistas
+{ 
+    public class UnityBarraDeProgresoVista : MonoBehaviour
+    {
+        [SerializeField] Slider barraDeProgreso;
+        [SerializeField] Gradient gradiente;
+        [SerializeField] Image colorDeLaBarra;
+        
+        void Awake()
+        {
+            barraDeProgreso.maxValue = 10;
+            barraDeProgreso.value = 10;
+            colorDeLaBarra.color = gradiente.Evaluate(1f);
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                DescontarEnLaBarraDeProgreso();
+        }
+
+        void DescontarEnLaBarraDeProgreso()
+        {
+            barraDeProgreso.value -= 1;
+            colorDeLaBarra.color = gradiente.Evaluate(barraDeProgreso.normalizedValue);
+        }
+    }
+}
