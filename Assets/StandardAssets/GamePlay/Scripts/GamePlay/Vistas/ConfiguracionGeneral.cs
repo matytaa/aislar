669270a9bd80133873b5e1568745dba074fb5071
Matias Nievas Soto <matytaa@gmail.com>
2021-01-16 01:36:39 -0300

    Resolucion de conflictos

---
+++ Assets/StandardAssets/GamePlay/Scripts/GamePlay/Vistas/ConfiguracionGeneral.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StandardAssets.GamePlay.Scripts.GamePlay.Vistas
+{
+    [CreateAssetMenu(fileName = "Configuracion general", menuName = "Aislar/Crear configuracion general con niveles")]
+    public class ConfiguracionGeneral : ScriptableObject
+    {
+        [SerializeField] List<ConfiguracionDelNivel> listaDeConfiguracionDeNiveles;
+
+        public List<ConfiguracionDelNivel> DarConfiguracionDeNiveles()
+        {
+            return listaDeConfiguracionDeNiveles;
+        }
+    }
+}
